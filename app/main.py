b26c3a701b09e6f7539812167ecb73810afead77
Irfan Baharuddin <irfannaqieb@gmail.com>
2025-10-13 21:39:04 +0900

    update CORS origins and requirements.txt

--- app/main.py
+++ app/main.py
@@ -11,8 +11,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://*.up.railway.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
