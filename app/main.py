6b09d550e5bf62ec641c49155b445095a8217ce5
Irfan Baharuddin <irfannaqieb@gmail.com>
2025-10-13 21:58:58 +0900

    add deployed frontend url to CORS

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://*.up.railway.app",
+        "https://capstone-frontend-rho.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
